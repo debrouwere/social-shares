2a38f7fe6431ae3cda2426d3f73186be84451ab6
Stijn Debrouwere <stijn@debrouwere.org>
2014-11-29 20:05:43 +0100

    Tweak Facebook Graph API request.

--- socialshares/platforms/facebook.py
+++ socialshares/platforms/facebook.py
@@ -1,11 +1,11 @@
 import grequests
 
 def fetch(url):
-    return grequests.get('https://graph.facebook.com/', params={'ids': url})
+    return grequests.get('https://graph.facebook.com/', params={'id': url})
 
 def parse(response):
     if response.status_code != 200:
         raise IOError()
 
-    data = response.json().values()[0]
+    data = response.json()
     return data['shares']
